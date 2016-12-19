26fea8eaa1b345e4f1a03b2e8f6e18034f813aea
OranShuster <thristle666@gmail.com>
2016-12-19 18:13:50 +0200

    Init

---
+++ Assets/Scripts/ShapesManager.cs
@@ -0,0 +1,410 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.IO; 
+
+
+public class ShapesManager : MonoBehaviour
+{
+    public Text DebugText, ScoreText;
+    public bool ShowDebugInfo = false;
+    //candy graphics taken from http://opengameart.org/content/candy-pack-1
+
+    public ShapesArray shapes;
+
+    private int score;
+	private int seriesDelta = 1;
+	private int numberStyle = 0;
+
+    public readonly Vector2 BottomRight = new Vector2(-2.37f, -4.27f);
+    public readonly Vector2 CandySize = new Vector2(0.85f, 0.85f);
+
+    private GameState state = GameState.None;
+    private GameObject hitGo = null;
+    private Vector2[] SpawnPositions;
+    public GameObject numberSquare_prefab;
+    public GameObject[] ExplosionPrefabs;
+
+	public static int MAX_NUMBER;
+	public static int Rows;
+	public static int Columns;
+	private Sprite[] numberSquare_sprites;
+    //public GameObject[] BonusPrefabs;
+
+    //private IEnumerator CheckPotentialMatchesCoroutine;
+    //private IEnumerator AnimatePotentialMatchesCoroutine;
+
+    //IEnumerable<GameObject> potentialMatches;
+
+    public SoundManager soundManager;
+    void Awake()
+    {
+        DebugText.enabled = ShowDebugInfo;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        //InitializeTypesOnPrefabShapesAndBonuses();
+		InitializeBoardConstants();
+        InitializeCandyAndSpawnPositions();
+
+        //StartCheckForPotentialMatches();
+    }
+
+	public void InitializeBoardConstants(){
+		var directory_information = new DirectoryInfo ("Assets/Resources/Images/Numbers");
+		var files_info = directory_information.GetFiles ("*.jpg");
+		MAX_NUMBER = files_info.Length;
+		Rows = MAX_NUMBER;
+		Columns = MAX_NUMBER;
+		numberSquare_sprites = Resources.LoadAll<Sprite> ("Images/Numbers");
+
+	}
+
+    public void InitializeCandyAndSpawnPositionsFromPremadeLevel()
+    {
+        InitializeVariables();
+
+        var premadeLevel = DebugUtilities.FillShapesArrayFromResourcesData();
+
+        if (shapes != null)
+            DestroyAllCandy();
+
+        shapes = new ShapesArray();
+        SpawnPositions = new Vector2[Columns];
+
+        for (int row = 0; row < Rows; row++)
+        {
+            for (int column = 0; column < Columns; column++)
+            {
+
+                GameObject newCandy = null;
+
+                newCandy = GetSpecificCandyForPremadeLevel(premadeLevel[row, column]);
+
+                InstantiateAndPlaceNewCandy(row, column, newCandy);
+
+            }
+        }
+
+        SetupSpawnPositions();
+    }
+
+
+    public void InitializeCandyAndSpawnPositions()
+    {
+        InitializeVariables();
+
+        if (shapes != null)
+            DestroyAllCandy();
+
+        shapes = new ShapesArray();
+        SpawnPositions = new Vector2[Columns];
+
+        for (int row = 0; row < Rows; row++)
+        {
+            for (int column = 0; column < Columns; column++)
+            {
+
+				GameObject newCandy = numberSquare_prefab;
+				var newCandyShape = newCandy.GetComponent<Shape>();
+				newCandyShape.Value=Random.Range (0, ShapesManager.MAX_NUMBER);
+
+                //check if two previous horizontal are of the same type
+                while (column >= 2 && shapes[row, column - 1].GetComponent<Shape>()
+                    .IsPartOfSeries(newCandy.GetComponent<Shape>(),seriesDelta)
+                    && shapes[row, column - 2].GetComponent<Shape>().IsPartOfSeries(newCandy.GetComponent<Shape>(),seriesDelta))
+                {
+					newCandyShape.Value = Random.Range (0, ShapesManager.MAX_NUMBER);
+                }
+
+                //check if two previous vertical are of the same type
+                while (row >= 2 && shapes[row - 1, column].GetComponent<Shape>()
+                    .IsPartOfSeries(newCandy.GetComponent<Shape>(),seriesDelta)
+                    && shapes[row - 2, column].GetComponent<Shape>().IsPartOfSeries(newCandy.GetComponent<Shape>(),seriesDelta))
+                {
+					newCandyShape.Value = Random.Range (0, ShapesManager.MAX_NUMBER);
+                }
+
+                InstantiateAndPlaceNewCandy(row, column, newCandy);
+
+            }
+        }
+
+        SetupSpawnPositions();
+    }
+
+
+
+    private void InstantiateAndPlaceNewCandy(int row, int column, GameObject newCandy)
+    {
+        GameObject go = Instantiate(newCandy,
+            BottomRight + new Vector2(column * CandySize.x, row * CandySize.y), Quaternion.identity)
+            as GameObject;
+
+		int number_value = newCandy.GetComponent<Shape> ().Value;
+        //assign the specific properties
+		go.GetComponent<Shape>().Assign(number_value, row, column);
+		go.GetComponent<SpriteRenderer> ().sprite =numberSquare_sprites[number_value];
+        shapes[row, column] = go;
+    }
+
+    private void SetupSpawnPositions()
+    {
+        //create the spawn positions for the new shapes (will pop from the 'ceiling')
+        for (int column = 0; column < Columns; column++)
+        {
+            SpawnPositions[column] = BottomRight
+                + new Vector2(column * CandySize.x, Rows * CandySize.y);
+        }
+    }
+
+
+
+
+    /// <summary>
+    /// Destroy all candy gameobjects
+    /// </summary>
+    private void DestroyAllCandy()
+    {
+        for (int row = 0; row < Rows; row++)
+        {
+            for (int column = 0; column < Columns; column++)
+            {
+                Destroy(shapes[row, column]);
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ShowDebugInfo)
+            DebugText.text = DebugUtilities.GetArrayContents(shapes);
+
+        if (state == GameState.None)
+        {
+            //user has clicked or touched
+            if (Input.GetMouseButtonDown(0))
+            {
+                //get the hit position
+                var hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+                if (hit.collider != null) //we have a hit!!!
+                {
+                    hitGo = hit.collider.gameObject;
+                    state = GameState.SelectionStarted;
+                }
+                
+            }
+        }
+        else if (state == GameState.SelectionStarted)
+        {
+            //user dragged
+            if (Input.GetMouseButton(0))
+            {
+                
+
+                var hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+                //we have a hit
+                if (hit.collider != null && hitGo != hit.collider.gameObject)
+                {
+
+                    //if the two shapes are diagonally aligned (different row and column), just return
+                    if (!Utilities.AreVerticalOrHorizontalNeighbors(hitGo.GetComponent<Shape>(),
+                        hit.collider.gameObject.GetComponent<Shape>()))
+                    {
+                        state = GameState.None;
+                    }
+                    else
+                    {
+                        state = GameState.Animating;
+                        FixSortingLayer(hitGo, hit.collider.gameObject);
+                        StartCoroutine(FindMatchesAndCollapse(hit));
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Modifies sorting layers for better appearance when dragging/animating
+    /// </summary>
+    /// <param name="hitGo"></param>
+    /// <param name="hitGo2"></param>
+    private void FixSortingLayer(GameObject hitGo, GameObject hitGo2)
+    {
+        SpriteRenderer sp1 = hitGo.GetComponent<SpriteRenderer>();
+        SpriteRenderer sp2 = hitGo2.GetComponent<SpriteRenderer>();
+        if (sp1.sortingOrder <= sp2.sortingOrder)
+        {
+            sp1.sortingOrder = 1;
+            sp2.sortingOrder = 0;
+        }
+    }
+
+
+
+
+    private IEnumerator FindMatchesAndCollapse(RaycastHit2D hit2)
+    {
+        //get the second item that was part of the swipe
+        var hitGo2 = hit2.collider.gameObject;
+        shapes.Swap(hitGo, hitGo2);
+
+        //move the swapped ones
+        hitGo.transform.positionTo(Constants.AnimationDuration, hitGo2.transform.position);
+        hitGo2.transform.positionTo(Constants.AnimationDuration, hitGo.transform.position);
+        yield return new WaitForSeconds(Constants.AnimationDuration);
+
+        //get the matches via the helper methods
+        var hitGomatchesInfo = shapes.GetMatches(hitGo);
+        var hitGo2matchesInfo = shapes.GetMatches(hitGo2);
+
+        var totalMatches = hitGomatchesInfo.MatchedCandy
+            .Union(hitGo2matchesInfo.MatchedCandy).Distinct();
+
+        int timesRun = 1;
+        while (totalMatches.Count() >= Constants.MinimumMatches)
+        {
+            //increase score
+            IncreaseScore((totalMatches.Count() - 2) * Constants.Match3Score);
+
+            if (timesRun >= 2)
+                IncreaseScore(Constants.SubsequentMatchScore);
+
+            soundManager.PlayCrincle();
+
+            foreach (var item in totalMatches)
+            {
+                shapes.Remove(item);
+                RemoveFromScene(item);
+            }
+				
+            //get the columns that we had a collapse
+            var columns = totalMatches.Select(go => go.GetComponent<Shape>().Column).Distinct();
+
+            //the order the 2 methods below get called is important!!!
+            //collapse the ones gone
+            var collapsedCandyInfo = shapes.Collapse(columns);
+            //create new ones
+            var newCandyInfo = CreateNewCandyInSpecificColumns(columns);
+
+            int maxDistance = Mathf.Max(collapsedCandyInfo.MaxDistance, newCandyInfo.MaxDistance);
+
+            MoveAndAnimate(newCandyInfo.AlteredCandy, maxDistance);
+            MoveAndAnimate(collapsedCandyInfo.AlteredCandy, maxDistance);
+
+            //will wait for both of the above animations
+            yield return new WaitForSeconds(Constants.MoveAnimationMinDuration * maxDistance);
+
+            //search if there are matches with the new/collapsed items
+            totalMatches = shapes.GetMatches(collapsedCandyInfo.AlteredCandy).
+                Union(shapes.GetMatches(newCandyInfo.AlteredCandy)).Distinct();
+
+            timesRun++;
+        }
+
+        state = GameState.None;
+    }
+
+    /// <summary>
+    /// Spawns new candy in columns that have missing ones
+    /// </summary>
+    /// <param name="columnsWithMissingCandy"></param>
+    /// <returns>Info about new candies created</returns>
+    private AlteredCandyInfo CreateNewCandyInSpecificColumns(IEnumerable<int> columnsWithMissingCandy)
+    {
+        AlteredCandyInfo newCandyInfo = new AlteredCandyInfo();
+
+        //find how many null values the column has
+        foreach (int column in columnsWithMissingCandy)
+        {
+            var emptyItems = shapes.GetEmptyItemsOnColumn(column);
+            foreach (var item in emptyItems)
+            {
+				var go = numberSquare_prefab;
+				var newCandyShape = go.GetComponent<Shape> ();
+				newCandyShape.Value = Random.Range (0, ShapesManager.MAX_NUMBER);
+
+                GameObject newCandy = Instantiate(go, SpawnPositions[column], Quaternion.identity)
+                    as GameObject;
+				
+				newCandy.GetComponent<Shape>().Assign(go.GetComponent<Shape>().Value, item.Row, item.Column);
+
+				if (ShapesManager.MAX_NUMBER - item.Row > newCandyInfo.MaxDistance)
+					newCandyInfo.MaxDistance = ShapesManager.MAX_NUMBER - item.Row;
+
+                shapes[item.Row, item.Column] = newCandy;
+                newCandyInfo.AddCandy(newCandy);
+            }
+        }
+        return newCandyInfo;
+    }
+
+    /// <summary>
+    /// Animates gameobjects to their new position
+    /// </summary>
+    /// <param name="movedGameObjects"></param>
+    private void MoveAndAnimate(IEnumerable<GameObject> movedGameObjects, int distance)
+    {
+        foreach (var item in movedGameObjects)
+        {
+            item.transform.positionTo(Constants.MoveAnimationMinDuration * distance, BottomRight +
+                new Vector2(item.GetComponent<Shape>().Column * CandySize.x, item.GetComponent<Shape>().Row * CandySize.y));
+        }
+    }
+
+    /// <summary>
+    /// Destroys the item from the scene and instantiates a new explosion gameobject
+    /// </summary>
+    /// <param name="item"></param>
+    private void RemoveFromScene(GameObject item)
+    {
+        GameObject explosion = GetRandomExplosion();
+        var newExplosion = Instantiate(explosion, item.transform.position, Quaternion.identity) as GameObject;
+        Destroy(newExplosion, Constants.ExplosionDuration);
+        Destroy(item);
+    }
+
+    private void InitializeVariables()
+    {
+        score = 0;
+        ShowScore();
+    }
+
+    private void IncreaseScore(int amount)
+    {
+        score += amount;
+        ShowScore();
+    }
+
+    private void ShowScore()
+    {
+        ScoreText.text = "Score: " + score.ToString();
+    }
+
+    /// <summary>
+    /// Get a random explosion
+    /// </summary>
+    /// <returns></returns>
+    private GameObject GetRandomExplosion()
+    {
+        return ExplosionPrefabs[Random.Range(0, ExplosionPrefabs.Length)];
+    }
+
+    /// <summary>
+    /// Gets a specific candy or Bonus based on the premade level information.
+    /// </summary>
+    /// <param name="info"></param>
+    /// <returns></returns>
+    private GameObject GetSpecificCandyForPremadeLevel(string info)
+    {
+		var newCandy = numberSquare_prefab;
+		newCandy.GetComponent<Shape>().Value = int.Parse(info);
+		return newCandy;
+    }
+}

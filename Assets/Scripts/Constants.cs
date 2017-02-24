1cb019496f6cefa65b6a558468713422e191e734
Oran Shuster <oranshuster@gmail.com>
2017-02-24 16:04:22 +0200

    Mainmenu and Timer progress bar added

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,23 +1,27 @@
-﻿public static class Constants
+﻿using System.Net.NetworkInformation;
+
+public static class Constants
 {
-        public static readonly float AnimationDuration =  0.2f;
+    public static readonly float AnimationDuration =  0.2f;
 
-        public static readonly float MoveAnimationMinDuration = 0.05f;
+    public static readonly float MoveAnimationMinDuration = 0.05f;
 
-        public static readonly float ExplosionDuration = 0.3f;
+    public static readonly float ExplosionDuration = 0.3f;
 
-        public static readonly float WaitBeforePotentialMatchesCheck = 2f;
-        public static readonly float OpacityAnimationFrameDelay = 0.05f;
+    public static readonly float WaitBeforePotentialMatchesCheck = 2f;
+    public static readonly float OpacityAnimationFrameDelay = 0.05f;
 
-        public static readonly int MinimumMatches = 3;
+    public static readonly int MinimumMatches = 3;
 
-        public static readonly float StartingGameTimer = 30;
-        public static readonly int NextLevelScore = 1000;
+    public static readonly float StartingGameTimer = 30;
+    public static readonly int NextLevelScore = 1000;
 
-        public static UnityEngine.Color ColorBase = new UnityEngine.Color(236 / 255f, 228 / 255f, 217 / 255f);
-        public static UnityEngine.Color ColorSelected = new UnityEngine.Color(242 / 255f, 177 / 255f, 121 / 255f);
-        public static UnityEngine.Color ColorMatched = new UnityEngine.Color(236 / 255f, 206 / 255f, 110 / 255f);
+    public static UnityEngine.Color ColorBase = new UnityEngine.Color(236 / 255f, 228 / 255f, 217 / 255f);
+    public static UnityEngine.Color ColorSelected = new UnityEngine.Color(242 / 255f, 177 / 255f, 121 / 255f);
+    public static UnityEngine.Color ColorMatched = new UnityEngine.Color(236 / 255f, 206 / 255f, 110 / 255f);
 
+    public static float TimerMax = 100;
+    public static float TimerLow = 10;
 
 }
 

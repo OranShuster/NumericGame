cdff6d5edfa291b78e10116e6bf97bf78bd74972
Oran Shuster <oranshuster@gmail.com>
2017-03-03 19:50:00 +0200

    Added userinformation logic

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
@@ -12,46 +13,66 @@ public class MainMenuController : MonoBehaviour
     public GameObject MainMenuPanel;
     public GameObject UserCodePanel;
     public InputField UserCodeInputField;
+    public GameObject PlayStatsView;
+    public GameObject PlayDateStats;
+    public GameObject PlayStatsViewContent;
 
     private CanvasGroup _mainMenuGroup;
     private CanvasGroup _userCodeGroup;
-	// Use this for initialization
-	void Start ()
-	{
-	    _mainMenuGroup = MainMenuPanel.GetComponent<CanvasGroup>();
-	    _userCodeGroup = UserCodePanel.GetComponent<CanvasGroup>();
-	}
-
-    // Update is called once per frame
-    void Update () {
-		
-	}
-
-    public void CodeSubmitButtonClick()
+    private UserInformation _userInfo;
+    // Use this for initialization
+    void Start()
     {
-        if (SendCodeToServer(UserCodeInputField.text))
+        _mainMenuGroup = MainMenuPanel.GetComponent<CanvasGroup>();
+        _userCodeGroup = UserCodePanel.GetComponent<CanvasGroup>();
+        if (UserInformation.PlayerDataExists())
         {
+            _userInfo = new UserInformation("");
             HideUserInputElements();
             ShowMainMenu();
         }
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CodeSubmitButtonClick()
+    {
+        if (UserInformation.PlayerDataExists())
+            _userInfo = new UserInformation();
+        else
+            try
+            {
+                _userInfo = new UserInformation(UserCodeInputField.text);
+            }
+            catch (Exception)
+            {
+                Debug.Log("Error getting user info from server");
+            }
+        if (_userInfo == null)
+        {
+            Debug.Log("Error loading user data");
+            return;
+        }
+        HideUserInputElements();
+        ShowMainMenu();
     }
 
     private void ShowMainMenu()
     {
         _mainMenuGroup.ZKalphaTo(1f).start();
         _mainMenuGroup.interactable = true;
+        foreach (PlayDate date in _userInfo)
+            addDateToScrollView(date);
     }
 
     private void HideUserInputElements()
     {
         _userCodeGroup.ZKalphaTo(0f).start();
         _userCodeGroup.interactable = false;
-    }
-
-    private bool SendCodeToServer(string text)
-    {
-        return true;
     }
 
     public void StartGame()
@@ -61,6 +82,23 @@ public class MainMenuController : MonoBehaviour
 
     public void ShowPlayTimeStatistics()
     {
-        
+        PlayStatsView.SetActive(!PlayStatsView.activeInHierarchy);
+    }
+
+    private void addDateToScrollView(PlayDate date)
+    {
+        int numberOfChildren = PlayStatsViewContent.transform.childCount;
+        RectTransform dateStatsRect = PlayDateStats.transform as RectTransform;
+        float yLoc = numberOfChildren * dateStatsRect.rect.height + dateStatsRect.rect.height / 2;
+        GameObject go = Instantiate(PlayDateStats, new Vector2(500,-yLoc),Quaternion.identity);
+        go.layer = 5;
+        go.transform.SetParent(PlayStatsViewContent.transform,false);
+        Text dateString = go.transform.Find("Date").gameObject.GetComponent<Text>();
+        Text sessionsString = go.transform.Find("Sessions").gameObject.GetComponent<Text>();
+        Text curSessionTime = go.transform.Find("CurrentSessionTime").gameObject.GetComponent<Text>();
+        dateString.text = date.SessionDate;
+        sessionsString.text = String.Format("{0}/{1}", Math.Min(date.CurrentSession, date.NumOfSessions), date.NumOfSessions);
+        curSessionTime.text = String.Format("{0}/{1}", date.CurrentSessionTime, date.SessionPlayTime);
+
     }
 }
---
+++ Assets/Scripts/UserInformation.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Newtonsoft.Json.Serialization;
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Reflection;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+public class PlayDate
+{
+    public string SessionDate { get; set; }
+    public int NumOfSessions { get; set; }
+    public int SessionPlayTime { get; set; }
+    public int CurrentSession = 1;
+    public int CurrentSessionTime = 0;
+}
+
+public class UserInformation : IEnumerable
+{
+    public PlayDate[] UserPlayDates=new PlayDate[0];
+    static string userDataPath = Application.persistentDataPath + "/userData.cjd";
+    public UserInformation(string userCode)
+    {
+        if (userCode == "")
+            UserPlayDates = Load();
+        else
+        {
+            string json = GetJsonFromServer(userCode);
+            UserPlayDates = JsonConvert.DeserializeObject<PlayDate[]>(json);
+        }
+    }
+    public UserInformation(){}
+
+    public static string GetJsonFromServer(string userCode)
+    {
+        throw new NotImplementedException("GetJsonFromServer");
+    }
+
+    public int CanPlay(DateTime playTime)
+    {
+        var todayDate = DateTime.Today.ToString(Constants.DateFormat);
+        PlayDate todayDateEntry = Array.Find(UserPlayDates, x => x.SessionDate == todayDate);
+        if (todayDateEntry != null)
+            if (todayDateEntry.CurrentSession <= todayDateEntry.NumOfSessions)
+                return todayDateEntry.SessionPlayTime-todayDateEntry.CurrentSessionTime;
+        return 0;
+    }
+
+    public void Save()
+    {
+        Debug.Log(String.Format("Saving user data from {0}", userDataPath));
+        XmlSerializer saver = new XmlSerializer(UserPlayDates.GetType());
+        TextWriter tw = new StreamWriter(new FileStream(userDataPath, FileMode.OpenOrCreate));
+        saver.Serialize(tw,UserPlayDates);
+    }
+
+    public PlayDate[] Load()
+    {
+        Debug.Log(String.Format("Loading user data from {0}",userDataPath));
+        if (PlayerDataExists())
+        {
+            XmlSerializer loader = new XmlSerializer(UserPlayDates.GetType());
+            TextReader tr = new StreamReader(new FileStream(userDataPath, FileMode.Open));
+            return (PlayDate[])loader.Deserialize(tr);
+        }
+        Debug.Log("User data not found. wrong constructor used");
+        return null;
+    }
+
+    public static bool PlayerDataExists()
+    {
+        return File.Exists(userDataPath);
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        return UserPlayDates.GetEnumerator();
+    }
+}

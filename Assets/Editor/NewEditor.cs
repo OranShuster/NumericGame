5002a0ffa90d7ca1dc328545f12d1a13c51a300e
Oran Shuster <oranshuster@gmail.com>
2017-06-21 12:46:03 +0300

    New command line build option

---
+++ Assets/Editor/NewEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+
+public class BuildScript
+{
+    static void Build_Android()
+    {
+        BuildPlayerOptions buildPlayerOptions =
+            new BuildPlayerOptions
+            {
+                scenes = new[]
+                {
+                    "Assets/Scenes/UserRegistration.unity", "Assets/Scenes/MainMenu.unity",
+                    "Assets/Scenes/Tutorial.unity", "Assets/Scenes/mainGame.unity"
+                },
+                locationPathName = "apk",
+                target = BuildTarget.Android,
+                options = BuildOptions.None
+            };
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+}
